e926ffc26e35fcfae7db4b7269a9463e6b694f1c
baul <roidinev@gmail.com>
2015-06-04 00:14:44 +0800

    bring filemap filesize to filename dictionary

--- Core.sh
+++ Core.sh
@@ -32,35 +32,47 @@ c=0
 while [ $c -lt 3 ]
 do
      (( c++ ))
-
+for filesize in 1024 65536 131072 196608 262144 327680 393216 458752 524288 589824 655360 786432 1048576
 #for filesize in 612 1024 524288 1048576 10485760 104857600
-for filesize in 612 1024
+#for filesize in 612 1024
+#for filesize in 524288 1048576
+#for filesize in 10485760 104857600
 do
-    #bitnum=$(`echo $filesize |wc -c`)
-    bitnum=`echo $filesize |wc -c`
-    case "$bitnum" in
-          4)
-           cp /usr/share/nginx/html/612B /usr/share/nginx/html/index.html
-          ;;
-          5)
-           cp /usr/share/nginx/html/1KB /usr/share/nginx/html/index.html
-          ;;
-          7)
-           cp /usr/share/nginx/html/512KB /usr/share/nginx/html/index.html
-          ;;
-          8)
-           cp /usr/share/nginx/html/1MB /usr/share/nginx/html/index.html
-          ;;
-          9)
-           cp /usr/share/nginx/html/10MB /usr/share/nginx/html/index.html
-          ;;
-          10)
-           cp /usr/share/nginx/html/100MB /usr/share/nginx/html/index.html
-          ;;
-          *)
-           echo "not needed file size "
-          ;;
-    esac
+
+     while read sizekey filename; do
+
+     if [ $sizekey == $filesize ] ;then
+     cp /usr/share/nginx/html/$filename /usr/share/nginx/html/index.html
+#     ls -la  /usr/share/nginx/html/index.html
+     fi
+     done < /root/RUNs/filemap
+#above method also not ideal ,wget use B uit,so 612B file cannot deal,we use keyvalue or csv.
+
+#    #bitnum=$(`echo $filesize |wc -c`) why wrong
+#    bitnum=`echo $filesize |wc -c` we need a unverisal method to process diffenrent size,B KB MB 
+#    case "$bitnum" in
+#          4)
+#           cp /usr/share/nginx/html/612B /usr/share/nginx/html/index.html
+#          ;;
+#          5)
+#           cp /usr/share/nginx/html/1KB /usr/share/nginx/html/index.html
+#          ;;
+#          7)
+#           cp /usr/share/nginx/html/512KB /usr/share/nginx/html/index.html
+#          ;;
+#          8)
+#           cp /usr/share/nginx/html/1MB /usr/share/nginx/html/index.html
+#          ;;
+#          9)
+#           cp /usr/share/nginx/html/10MB /usr/share/nginx/html/index.html
+#          ;;
+#          10)
+#           cp /usr/share/nginx/html/100MB /usr/share/nginx/html/index.html
+#          ;;
+#          *)
+#           echo "not needed file size "
+#          ;;
+#    esac
 
 #    cp /usr/share/nginx/index/$filesize /usr/share/nginx/index/index.html
     ansible -u root 10.10.10.254 -m shell -a "/usr/local/squid/bin/refresh_cli -f http://www.myweb.com/index.html"
